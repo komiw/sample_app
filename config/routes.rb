15c0a163381f8d515136157fc029beb17fe97957
MATC Student <student@MATC1234.(none)>
2012-12-05 18:56:00 -0600

    Basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 SampleApp::Application.routes.draw do
+
   get "users/new"
 
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
-
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact'  
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -19,7 +18,6 @@ SampleApp::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
@@ -65,4 +63,5 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
 end
